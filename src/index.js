682daf87e0845088406d3eb39005a705000e7893
iryna-zhyhala <iryna.zhyhala@gmail.com>
2023-02-01 00:44:51 +0300

    feat: add decision task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = '';
+  for (let i = 0; i < expr.length; i = i + 10) {
+    let encodedLetter = expr.substring(i, i + 10);
+    if (encodedLetter == '**********') {
+      result += ' ';
+      continue;
+    }
+    let key = '';
+    for (let j = 0; j < encodedLetter.length; j +=2) {
+      let temp = encodedLetter.substring(j, j + 2);
+      if (temp == '10') {
+        key += '.';
+      } else if (temp == '11') {
+        key += '-';
+      }
+    }
+    result += MORSE_TABLE[key];
+  }
+  return result;
 }
 
 module.exports = {
